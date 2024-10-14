28bfa950c44cc15e249f3e4715160421b9098ed8
Daniel Sullivan <daniel.sullivan7@msubillings.edu>
2024-10-14 14:31:39 -0600

    splash page initial commit

---
+++ BattleShip/src/SplashPageOptions.java
@@ -0,0 +1,42 @@
+public class SplashPageOptions {
+    public void displayTitle(){
+        System.out.print(
+        "                                                                            \n" +    
+        "██████   █████  ████████ ████████ ██      ███████ ███████ ██   ██ ██ ██████ \n" + 
+        "██   ██ ██   ██    ██       ██    ██      ██      ██      ██   ██ ██ ██   ██\n" +
+        "██████  ███████    ██       ██    ██      █████   ███████ ███████ ██ ██████ \n" +
+        "██   ██ ██   ██    ██       ██    ██      ██           ██ ██   ██ ██ ██     \n" + 
+        "██████  ██   ██    ██       ██    ███████ ███████ ███████ ██   ██ ██ ██     \n" +
+        "\n"
+        );
+    }
+
+    public void displayBattleshipArt() {
+        System.out.print(
+    "                                                                            \n" + 
+    "                                       # #  ( )                             \n" +
+    "                                    ___#_#___|__                            \n" +
+    "                                _  |____________|  _                        \n"+            
+    "                         _=====| | |            | | |==== _                 \n"+            
+    "                   =====| |.---------------------------. | |====            \n" +            
+    "     <--------------------'   .  .  .  .  .  .  .  .   '--------------/     \n" +            
+    "       \\                                                             /     \n" +            
+    "        \\_____________________________BATTLESHIP____________________/      \n" +            
+    "    wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww   \n" +            
+    "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwWWWW\n" +            
+    "    wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww     \n" +            
+    "\n");
+    }
+
+    public void displayGameOptions() {
+        System.out.print(
+            "\n" +
+            "To get started, choose one of the following options:\n" +
+            "\n" +
+            "1. Choose One Player Mode \n" +
+            "2. Choose Two Player Mode \n" +
+            "3. Display Tutorial       \n" +
+            "4. Start Game             \n" +
+            "\n"); 
+        }
+}
