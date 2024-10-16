8eb7917bbb12749d4ff860056a68d78c45038ec2
Charles Miller <charles.miller16@msubillings.edu>
2024-10-16 01:41:09 -0600

    Build out player class

--- src/player.java
+++ src/player.java
@@ -1,3 +1,36 @@
-public class player {
+import java.util.Scanner;
 
+public class player {
+    private String playerName;
+
+    // constructor that optionally accepts a name
+    public Player() {
+        this.playerName = "";
+    }
+
+    // getter method to retrieve the player's name
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    // setter method to set or update the player's name
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    // method to prompt the user for their name
+    public void promptForPlayerName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        setPlayerName(name);
+        System.out.printIn("Player name set to: " + getPlayerName());
+    }
+
+    // main testing method
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.promptForPlayerName();
+        // player name is now set and can be used
+    }
 }
