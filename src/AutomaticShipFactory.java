4d214cb557930a359779554c0d2bb5987ee026de
Princess <princess.natalie@msubillings.edu>
2024-10-28 13:51:33 -0600

    add automatic ship factory

---
+++ src/AutomaticShipFactory.java
@@ -0,0 +1,14 @@
+public class AutomaticShipFactory extends ShipFactory {
+
+
+    @Override
+    protected Coordinate getStartCoordinate(Ship ship) throws Exception {
+        return Coordinate.randomCoordinate();
+    }
+
+    @Override
+    protected Direction getStartDirection(Ship ship) throws Exception {
+        return Direction.randomDirection();
+    }
+
+}
