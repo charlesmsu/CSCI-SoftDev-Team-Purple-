e2d50bd61d0cd27a20d0245fd2102044dc2295cb
Daniel Sullivan <daniel.sullivan7@msubillings.edu>
2024-10-28 23:13:17 -0600

    Constants Class Updated

--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,5 @@
 import java.util.Map;
 
 public class Constants {
-    public static Map<String, Integer> shipSpecifications = Map.of("Carrier", 5, "BattleShip", 4, "Cruiser", 3, "Submarine", 3, "Destroyer", 2);
+    public static final Map<String, Integer> shipSpecifications = Map.of("Carrier", 5, "BattleShip", 4, "Cruiser", 3, "Submarine", 3, "Destroyer", 2);
 }
