d660b732ece82b8febc9057cc8ff8ec37aa1a0e8
cairomadr <cairo.morton@msubillings.edu>
2024-11-01 14:13:48 -0600

    fix bug

--- test/GameTest.java
+++ test/GameTest.java
@@ -2,6 +2,6 @@ public class GameTest {
     public static void main(String[] args) {
         Game game = new Game();
         System.out.println("Testing Game two-player setup and play...");
-        game.startTwoPlayerGame();  // This starts the two-player game loop
+        //game.startTwoPlayerGame();  // This starts the two-player game loop
     }
 }
