e857022da1bcbec7eccb4779c67d10b290bd52af
cairomadr <cairo.morton@msubillings.edu>
2024-11-08 14:11:33 -0700

    adding easy player

---
+++ src/EasyPlayer.java
@@ -0,0 +1,57 @@
+public class EasyPlayer implements IPlayer {
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public Coordinate takeShot() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'takeShot'");
+    }
+
+    @Override
+    public ShotResult receiveShot(Coordinate shot) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'receiveShot'");
+    }
+
+    @Override
+    public void receiveShotResult(Coordinate shot, ShotResult shotResult) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'receiveShotResult'");
+    }
+
+    @Override
+    public boolean shipsAreSunk() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shipsAreSunk'");
+    }
+
+    @Override
+    public void placeShips() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'placeShips'");
+    }
+
+    @Override
+    public void printOceanGrid() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printOceanGrid'");
+    }
+
+    @Override
+    public void printTargetGrid() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printTargetGrid'");
+    }
+
+    @Override
+    public String getRecentSunkShip() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRecentSunkShip'");
+    }
+    
+}
